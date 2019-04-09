85d3f0ecf7c07c77b8d003239f6b8d0020855d38
Rustam Galeev <Vincent-cat@mail.ru>
2019-04-10 03:15:34 +0600

    Добавьте файлы проекта.

---
+++ Dz_17/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Xml;
+
+
+namespace Dz_17
+{
+    [Serializable]
+    [XmlRoot(ElementName = "item")]
+    public class Item
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "link")]
+        public string Link { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "pubdate")]
+        public DateTime PubDate { get; set; }
+        public Item()
+        {
+
+        }
+    }
+}
---
+++ Dz_17/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Dz_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                XmlSerializer formatter = new XmlSerializer(typeof(List<Item>));
+
+                string url = "https://habrahabr.ru/rss/interesting/";
+                string xml;
+                using (WebClient client = new WebClient())
+                {
+                    xml = client.DownloadString(url);
+                }
+
+                using (StreamWriter writer = new StreamWriter("file.xml", false, System.Text.Encoding.Default))
+                {
+                    writer.Write(xml);
+                }
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load("file.xml");
+
+                XmlNodeList items = doc.SelectNodes("rss/channel/item");
+                XmlNodeList titles = doc.SelectNodes("rss/channel/item/title");
+                XmlNodeList links = doc.SelectNodes("rss/channel/item/link");
+                XmlNodeList descriptions = doc.SelectNodes("rss/channel/item/description");
+                XmlNodeList pubDates = doc.SelectNodes("rss/channel/item/pubDate");
+
+                List<Item> list = new List<Item>();
+                for (int i = 0; i < items.Count; i++)
+                {
+                    Item item = new Item();
+                    item.Title = titles[i].InnerText;
+                    item.Link = links[i].InnerText;
+                    item.Description = descriptions[i].InnerText;
+                    item.PubDate = DateTime.Parse(pubDates[i].InnerText);
+                    list.Add(item);
+                }
+            }
+        }
+    }
+
